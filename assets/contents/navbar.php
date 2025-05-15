0bc3a6f391570c56c115676eb3d477895923d7f1
Aslam Vrtly <aslamviruthully01@gmail.com>
2025-05-15 22:21:36 +0530

    ptoduct hunt

--- assets/contents/navbar.php
+++ assets/contents/navbar.php
@@ -5,7 +5,7 @@
         </a>
         <div>
             <a href="https://www.producthunt.com/posts/webonitor?embed=true&utm_source=badge-featured&utm_medium=badge&utm_source=badge-webonitor" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=965558&theme=light&t=1747326071894" alt="Webonitor - A&#0032;tool&#0032;to&#0032;monitor&#0032;website&#0032;status&#0044;&#0032;domain&#0032;expiry&#0044;&#0032;etc | Product Hunt" style="width: 180px;" width="180" /></a>
-            <a href="/#support" class="btn btn-sm px-3 py-2">Support <i class="bi bi-heart"></i></a>
+            <a href="#support" class="btn btn-sm px-3 py-2">Support <i class="bi bi-heart"></i></a>
         </div>
     </div>
 </nav>
